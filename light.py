d0fb74dd528d3489f3c18aeac07e08f292770a7a
Adrian <35149617+Tinuviels@users.noreply.github.com>
2018-01-05 21:52:06 +0100

    get light

---
+++ light.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import smbus
+import time
+
+address = 0x48
+A0 = 0x40
+A1 = 0x41
+A2 = 0x42
+A3 = 0x43
+bus = smbus.SMBus(1)
+bus.write_byte(address,A0)
+value = bus.read_byte(address)
+print("%1.3f  " %(value*3.3/255))
